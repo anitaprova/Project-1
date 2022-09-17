90d914f04d443b50da0e248a924508079643af63
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-17 15:47:59 -0400

    finished

--- direction_reader.cpp
+++ direction_reader.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+	string s;
+	while (cin >> s) {
+		if (s == "R") {
+			cout << "Right" << endl;
+		}
+
+		if (s == "L") {
+			cout << "Left" << endl;
+		}
+
+		if (s == "U") {
+			cout << "Up" << endl;
+		}
+
+		if (s == "D") {
+			cout << "Down" << endl;
+		}
+	}	
+}
