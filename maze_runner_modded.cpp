db444f9441548499e588aa3832fee2ce6377ece2
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-30 18:02:38 -0400

    added file

---
+++ maze_runner_modded.cpp
@@ -0,0 +1,22 @@
+/*
+ Author: Anita Prova
+ Course: CSCI-135
+ Instructor: Genady Maryash
+ Assignment: Project 1 Task 4
+ The program prints whether you are out of the custom maze or not.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int maze[10][10];
+
+	string x;
+	int end_row, end_col, curr_row, curr_col;
+
+	while(cin >> x) {
+		cout << x << " ";
+	}
+}
